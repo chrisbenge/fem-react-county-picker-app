16ff24ee33bb066f6f318ccdf07b60412b7c0455
Chris Benge <chrisbenge@gmail.com>
2019-08-24 19:42:44 +0100

    Basic Set-up, API call and listing

--- country-picker/src/App.js
+++ country-picker/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setCountries ] = useState({countries: []});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json());
+      setCountries({countries: data}); 
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.countries.map((country) => 
+          <div key={country.alpha3Code}>
+            <img src={country.flag} alt={country.name} />
+            {country.name}
+          </div>
+        )
+      }
     </div>
   );
 }
